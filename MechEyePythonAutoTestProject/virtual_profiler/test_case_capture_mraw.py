dc684faf6f95fcee878e4ce52148a94216e2d24a
Lianchangle <lsiricreat@163.com>
2024-01-24 16:33:46 +0800

    no message

--- MechEyePythonAutoTestProject/virtual_profiler/test_case_capture_mraw.py
+++ MechEyePythonAutoTestProject/virtual_profiler/test_case_capture_mraw.py
@@ -1,4 +1,6 @@
 import os
+import time
+
 import cv2
 from base_test_case import *
 from multiprocessing import RLock
@@ -51,38 +53,43 @@ class TestCapture(unittest.TestCase):
 
         cv2.imwrite(os.path.join(self.current_cwd, "files/depth.tiff"), total_batch.get_depth_map().data())
         cv2.imwrite(os.path.join(self.current_cwd, "files/IntensityImage.tiff"), total_batch.get_intensity_image().data())
+        stop_status = self.virtual_profiler.stop_acquisition()
 
         self.mech_mind_status(start_status)
+        self.mech_mind_status(stop_status)
         self.mech_mind_status(data_status)
         self.mech_mind_status(line_status)
         self.assertEqual(data_width, 4096)
         self.assertGreaterEqual(line_count, 16)
         self.assertLessEqual(line_count, 20000)
 
-    def test_case_capture_with_call_back(self):
-        data_status, data_width = self.user_set.get_int_value(DataPointsPerProfile.name)
-        line_status, line_count = self.user_set.get_int_value(ScanLineCount.name)
-        total_batch = ProfileBatch(data_width)
-        callback = CustomAcquisitionCallback(data_width)
-        call_back_status = self.virtual_profiler.register_acquisition_callback(callback)
-
-        start_status = self.virtual_profiler.start_acquisition()
-
-        while True:
-            with mutex:
-                if not callback.total_batch.is_empty():
-                    break
-
-        cv2.imwrite(os.path.join(self.current_cwd, "files/DepthMapUsingCallback.tiff"), callback.total_batch.get_depth_map().data())
-        cv2.imwrite(os.path.join(self.current_cwd, "files/IntensityImageUsingCallback.tiff"), callback.total_batch.get_intensity_image().data())
-
-        print(callback.total_batch.height())
-        self.mech_mind_status(start_status)
-        self.mech_mind_status(call_back_status)
-        self.mech_mind_status(data_status)
-        self.mech_mind_status(line_status)
-        self.assertEqual(data_width, 4096)
-        self.assertGreaterEqual(line_count, 16)
-        self.assertLessEqual(line_count, 20000)
-        self.assertGreaterEqual(callback.total_batch.height(), 16)
-        self.assertLessEqual(callback.total_batch.height(), 20000)
+    # def test_case_capture_with_call_back(self):
+    #     time.sleep(40)
+    #     data_status, data_width = self.user_set.get_int_value(DataPointsPerProfile.name)
+    #     line_status, line_count = self.user_set.get_int_value(ScanLineCount.name)
+    #     total_batch = ProfileBatch(data_width)
+    #     callback = CustomAcquisitionCallback(data_width)
+    #     call_back_status = self.virtual_profiler.register_acquisition_callback(callback)
+    #
+    #     stop_status = self.virtual_profiler.stop_acquisition()
+    #     start_status = self.virtual_profiler.start_acquisition()
+    #
+    #     while True:
+    #         with mutex:
+    #             if not callback.total_batch.is_empty():
+    #                 break
+    #
+    #     cv2.imwrite(os.path.join(self.current_cwd, "files/DepthMapUsingCallback.tiff"), callback.total_batch.get_depth_map().data())
+    #     cv2.imwrite(os.path.join(self.current_cwd, "files/IntensityImageUsingCallback.tiff"), callback.total_batch.get_intensity_image().data())
+    #
+    #     print(callback.total_batch.height())
+    #     self.mech_mind_status(start_status)
+    #     self.mech_mind_status(stop_status)
+    #     self.mech_mind_status(call_back_status)
+    #     self.mech_mind_status(data_status)
+    #     self.mech_mind_status(line_status)
+    #     self.assertEqual(data_width, 4096)
+    #     self.assertGreaterEqual(line_count, 16)
+    #     self.assertLessEqual(line_count, 20000)
+    #     self.assertGreaterEqual(callback.total_batch.height(), 16)
+    #     self.assertLessEqual(callback.total_batch.height(), 20000)
